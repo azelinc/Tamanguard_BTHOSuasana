bf00457fe357de427920367016fca1929949f609
azelinc <50617069+azelinc@users.noreply.github.com>
2026-05-05 15:26:07 +0800

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2W9t2MoGdW3N3l1Yk5N_tN0INfEae9I",
+  authDomain: "tamanguard-btho-suasana.firebaseapp.com",
+  projectId: "tamanguard-btho-suasana",
+  storageBucket: "tamanguard-btho-suasana.appspot.com",
+  messagingSenderId: "629279041008",
+  appId: "1:629279041008:web:5f71f4f5091d223e1a94ec"
+};
