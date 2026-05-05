422973d65313b26f81f74dc0436f78ab67c4bb2d
azelinc <50617069+azelinc@users.noreply.github.com>
2026-05-05 17:15:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDlsV0Fx__HwoyLEmEofsbK00qeWxjySyw",
   authDomain: "tamanguard-btho-suasana.firebaseapp.com",
-  projectId: "tamanguard-btho-suasana",
+  projectId: "mytaman-1db3a",
   storageBucket: "tamanguard-btho-suasana.appspot.com",
   messagingSenderId: "629279041008",
   appId: "1:629279041008:web:5f71f4f5091d223e1a94ec"
